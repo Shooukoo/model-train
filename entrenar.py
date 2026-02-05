bca1ea25b5ec6eed15c82428389b688f41227892
Shoo <snakexxs80@gmail.com>
2026-02-04 23:27:53 -0600

    Ultimo entranamiento, 700 fotos, 71%.

--- entrenar.py
+++ entrenar.py
@@ -8,24 +8,25 @@ def entrenar_profesional():
     print("El sistema generará variaciones de tus fotos automáticamente.")
 
     results = model.train(
-        data='datasets/frutas/images', # Tu carpeta confirmada
-        epochs=30,                     # Aumentamos épocas porque ahora es más difícil memorizar
+        data='datasets/frutas/images', 
+        epochs=50,                     # Aumentamos épocas porque ahora es más difícil memorizar
         imgsz=224,
         project='runs_zarzamora',
-        name='modelo_v5',
+        name='modelo_v7',
         device='cpu',                  # Cambia a 0 si tienes GPU
         
-        # --- SECCIÓN MÁGICA: DATA AUGMENTATION ---
-        # Estos valores generan nuevas fotos en cada vuelta (epoch)
-        degrees=15.0,      # Rotar ligeramente la foto (+/- 15°)
-        translate=0.1,     # Mover la foto a los lados (simula mal encuadre)
-        scale=0.5,         # Zoom in/out (simula estar cerca o lejos)
-        fliplr=0.5,        # Espejo horizontal (clave para hojas)
-        mosaic=1.0,        # Mezclar 4 fotos en 1 (ayuda muchísimo a generalizar)
-        hsv_h=0.015,       # Cambiar tono de color (simula diferentes soles/climas)
+        augment=True,                  # Activar aumento de datos
+        dropout=0.5,                   # Desactivar algunas neuronas al azar
+        patience=10,                   # Detener si no hay mejora en 10 épocas
+        degrees=15.0,      # Rotar ligeramente la foto 
+        translate=0.1,     # Mover la foto a los lados 
+        scale=0.5,         # Zoom in/out 
+        fliplr=0.5,        # Espejo horizontal 
+        mosaic=1.0,        # Mezclar 4 fotos en 1 
+        hsv_h=0.015,       # Cambiar tono de color 
         hsv_s=0.4,         # Cambiar saturación
         hsv_v=0.4,         # Cambiar brillo
-        erasing=0.4,       # Borrar pequeños pedazos (obliga a la IA a no fijarse en detalles tontos)
+        erasing=0.4,       # Borrar pequeños pedazos 
     )
     
     print("\n¡Entrenamiento listo!")
