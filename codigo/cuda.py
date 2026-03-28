4d8e32c387426e4c2031a526433f0740b02b68a7
Isidro Arredondo Camarena <isiarrcam_2004@hotmail.com>
2026-03-28 13:57:25 -0600

    Fix

--- codigo/cuda.py
+++ codigo/cuda.py
@@ -1,2 +1,5 @@
-import torch
+import torch, torchvision, torchaudio
 print("CUDA disponible:", torch.cuda.is_available())
+print(torch.__version__)
+print("torchvision:", torchvision.__version__)
+print("torchaudio:", torchaudio.__version__)
