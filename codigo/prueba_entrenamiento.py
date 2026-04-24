d01a0b1f275e31c3b8ac56bbc66a766662b1faee
Isidro Arredondo Camarena <isiarrcam_2004@hotmail.com>
2026-04-23 20:05:15 -0600

    Update: acomodo de las carpetas de train y predict y prueba con modelo YOLOV8m-seg (salio mal)

--- codigo/prueba_entrenamiento.py
+++ codigo/prueba_entrenamiento.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 if __name__ == '__main__':
     print("--- Cargando modelo Nano ---")
-    model = YOLO('yolo26s-seg.pt') 
+    model = YOLO('yolov8m-seg.pt') 
 
     print("--- Iniciando entrenamiento de prueba ---")
     results = model.train(data='dataset_seg/data.yaml', epochs=50, imgsz=640,device=0)
