7133af81dd6909125aa1a8f4f15d050047d3bc2b
Isidro Arredondo <isiarrcam_2004@hotmail.com>
2026-06-23 16:33:14 -0600

    Fix leve

--- codigo/cuda.py
+++
@@ -1,5 +0,0 @@
-import torch, torchvision, torchaudio
-print("CUDA disponible:", torch.cuda.is_available())
-print(torch.__version__)
-print("torchvision:", torchvision.__version__)
-print("torchaudio:", torchaudio.__version__)
