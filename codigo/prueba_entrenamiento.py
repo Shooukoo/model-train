89a0535defc63589744bf00a3813c966012a165c
Isidro Arredondo <isiarrcam_2004@hotmail.com>
2026-07-05 10:15:25 -0600

    Update: Modelo V2 (provisional sin los datasets combinados) mp50 >95%, mp50-95>80%

--- codigo/prueba_entrenamiento.py
+++ codigo/prueba_entrenamiento.py
@@ -2,9 +2,9 @@ from ultralytics import YOLO
 
 if __name__ == '__main__':
     print("--- Cargando modelo Nano ---")
-    model = YOLO('yolo26m-seg.pt') 
+    model = YOLO('yolo26s-seg.pt') 
 
     print("--- Iniciando entrenamiento de prueba ---")
-    results = model.train(data='dataset_seg/data.yaml', epochs=100, imgsz=640,device=0)
+    results = model.train(data='dataset_nvo/data.yaml', epochs=100, imgsz=640,batch=8,device=0)
 
     print("--- ¡Prueba finalizada con éxito! ---")
